fceaa6e9164dfd429f38f777b5ee49120b95478a
Singh, Ashish <Ashish.Singh@Teradata.com>
2017-03-16 16:35:56 -0400

    Add Kerberize docker container for MapR with hive
    Add Kerberize docker container for MapR with hive

--- teradatalabs/mapr52-hive-kerberized/files/bootstrap.sh
+++ teradatalabs/mapr52-hive-kerberized/files/bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# CONFIGURE MapR
+# CONFIGURE MAPR
 /opt/mapr/server/configure.sh  -N mycluster -Z localhost -C localhost -HS localhost -no-autostart
 
 # SETUP FLAT FILE /home/mapr/storagefile
@@ -12,16 +12,33 @@ dd if=/dev/zero of=/home/mapr/storagefile bs=1G count=10
 # CREATE HIVE PROXY USERS
 chmod 755 /opt/mapr/conf/proxy
 
-# START SERVICES
-service mapr-zookeeper start
-service mapr-warden start
-
 # CONFIGURE HIVE
 /opt/mapr/server/configure.sh -R
 
-# WAIT FOR WARDEN TO START ALL THE SERVICES
+# CONFIGURE KERBEROS
+/opt/mapr/server/configure.sh -secure -genkeys -C localhost -Z localhost -N mycluster -no-autostart
+
+# START KERBEROS SERVICES
+/sbin/service krb5kdc start
+/sbin/service kadmin start
+
+# START MAPR SERVICES
+service mapr-zookeeper start
+service mapr-warden start
+
+# WAIT FOR WARDEN TO START ALL  THE SERVICES
 sh /root/wardenTracker.sh
+
+# CREATE KERBEROS TICKET
+kinit -kt /opt/mapr/conf/mapr.keytab mapr/mycluster@LABS.TERADATA.COM
+
+# CREATE MAPR TICKET
+maprlogin kerberos -user mapr/mycluster@LABS.TERADATA.COM
 
 # RUN HDFS COMMANDS
 hadoop fs -mkdir /user/root /user/hive /user/hdfs /user/hive/warehouse /var /var/mapr /var/mapr/cluster /var/mapr/cluster/yarn /var/mapr/cluster/yarn/rm /var/mapr/cluster/yarn/rm/staging /var/mapr/cluster/yarn/rm/staging/hive
 hadoop fs -chmod 777 /user/hive /user/hdfs /user/hive/warehouse /var/mapr /var/mapr/cluster/yarn/rm/staging/hive
+
+# REMOVE MAPR TICKET AND KERBEROS TICKET
+kdestroy
+rm -rf /tmp/*
