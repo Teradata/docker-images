e35f69280915fc2759a6e78fdf77bf5f264b976d
Singh, Ashish <Ashish.Singh@Teradata.com>
2017-04-06 10:27:08 -0400

    Adding Logic to wait for MapR ticket in Warden
    This changes will make maprcli wait for MapR ticket in wardenTracker

---
+++ teradatalabs/mapr52-hive-kerberized/files/wardenTracker.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+hname=$(hostname)
+
+Services=0
+maprloginReady=1
+maprcliReady=$(maprcli service list -node $hname | grep 'ERROR (10009)' | wc -l)
+
+# WAIT FOR CLDB TO START
+while [ $Services -ne 2 ]
+do
+if [ $maprcliReady == 1 ]
+then
+   maprcliReady=$(maprcli service list -node $hname | grep 'ERROR (10009)' | wc -l)
+   Services=0
+   # Get MapR ticket
+   if [ $maprloginReady -ne 0 ]
+   then
+     # CREATE KERBEROS TICKET
+     kinit -kt /opt/mapr/conf/mapr.keytab mapr/mycluster@LABS.TERADATA.COM
+       
+     # CREATE MAPR TICKET
+     maprlogin kerberos -user mapr/mycluster@LABS.TERADATA.COM
+     maprloginReady=$?
+   fi
+else
+   Services=$(maprcli service list -node $hname | grep CLDB |awk '{$1=$1};1' | tr ' ' '\n' | tail -1f)
+fi
+done
+
+# WAIT FOR NODEMANAGER TO START
+Services=0
+while [ $Services -ne 2 ]
+do
+   Services=$(maprcli service list -node $hname | grep NodeManager |awk '{$1=$1};1' | tr ' ' '\n' | tail -1f)
+done
+
+# WAIT FOR RESOURCEMANAGER TO START
+Services=0
+while [ $Services -ne 2 ]
+do
+   Services=$(maprcli service list -node $hname | grep ResourceManager |awk '{$1=$1};1' | tr ' ' '\n' | tail -1f)
+done
+
+# WAIT FOR HIVE METASTORE TO START
+Services=0
+while [ $Services -ne 2 ]
+do
+   Services=$(maprcli service list -node $hname | grep HiveMetastore |awk '{$1=$1};1' | tr ' ' '\n' | tail -1f)
+done
+
+# WAIT FOR HIVESERVER2 TO START
+Services=0
+while [ $Services -ne 2 ]
+do
+   Services=$(maprcli service list -node $hname | grep HiveServer2 |awk '{$1=$1};1' | tr ' ' '\n' | tail -1f)
+done
+
+# WAIT FOR JOBHISTORYSERVER TO START
+Services=0
+while [ $Services -ne 2 ]
+do
+   Services=$(maprcli service list -node $hname | grep JobHistoryServer |awk '{$1=$1};1' | tr ' ' '\n' | tail -1f)
+done
